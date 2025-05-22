6692e05e336ee31fa21e06ee62c103e97b247d16
aymanerihane <rihane.aymane@etu.uae.ac.ma>
2025-05-22 19:26:09 +0100

    push

--- spark-master/start_train.bat
+++ spark-master/start_train.bat
@@ -1,4 +1,6 @@
 @echo off
+docker exec spark-worker-2 pip install textblob   
+docker exec spark-worker-1 pip install textblob
 echo Starting Spark model training process...
 docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit --master spark://spark-master:7077 --deploy-mode client /train_model.py
 IF %ERRORLEVEL% NEQ 0 (
